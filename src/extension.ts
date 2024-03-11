d7721e333424b640ab7dbeb888c9c5867f2312a5
gayatri-wadhwani <gayatriwadhwani1@gmail.com>
2024-03-11 21:11:06 +0530

    Updated Logic for Capitalising first letter of comment

--- src/extension.ts
+++ src/extension.ts
@@ -1,38 +1,25 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
+// OTHERS //
 import * as vscode from "vscode";
 import { Parser } from "./parser";
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // This line of code will only be executed once when your extension is activated
-  console.log(
-    'Congratulations, your extension "capitalizenounincomments" is now active!'
-  );
-
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with registerCommand
   const parser = new Parser();
 
-  // Register commands here
+  // Register the command to capitalize comments
   let editAllCommentsCommand = vscode.commands.registerCommand(
-    "capitalizenounincomments.helloWorld",
-    () => {
+    "capitalo",
+    async () => {
       if (vscode.window.activeTextEditor) {
         const activeEditor = vscode.window.activeTextEditor;
+        // Set regex and find and capitalize comments
         parser.SetRegex(activeEditor, activeEditor.document.languageId);
         parser.findAndCapitalizeComments(activeEditor);
-        vscode.workspace.applyEdit(parser.edit).then((success) => {
-          if (success) {
-            vscode.window.showInformationMessage(
-              "Comments edited successfully!"
-            );
-          } else {
-            vscode.window.showErrorMessage("Failed to edit comments.");
-          }
-        });
+
+        // Apply the edit and save the document if successful
+        await vscode.workspace.applyEdit(parser.edit);
+        await vscode.commands.executeCommand("workbench.action.files.save");
       }
     }
   );
--- src/parser.ts
+++ src/parser.ts
@@ -3,57 +3,58 @@ import * as vscode from "vscode";
 import { checkNoun } from "./check.util";
 
 export class Parser {
-  private delimiters: string[] = [];
+  // Initialize class properties
+  private delimiters: { regexString: string; commentType: string }[] = [];
   private editRanges: boolean[] = [];
-
   public edit: vscode.WorkspaceEdit = new vscode.WorkspaceEdit();
   public uri: any;
   public supportedLanguage = true;
 
+  // Method to set regular expressions based on language code
   public SetRegex(activeEditor: vscode.TextEditor, languageCode: string) {
+    // If the language is supported, initialize workspace edit and URI
     if (this.setDelimiter(languageCode)) {
       this.edit = new vscode.WorkspaceEdit();
       this.uri = activeEditor.document.uri;
     } else {
+      // Show message if language is not supported
       vscode.window.showInformationMessage(
-        "Cannot edit comments : unknown language (" + languageCode + ")"
+        "Cannot edit comments: unknown language (" + languageCode + ")"
       );
     }
   }
 
-  /** Find and edit comments, capitalizing them */
   public findAndCapitalizeComments(activeEditor: vscode.TextEditor): void {
     let text = activeEditor.document.getText();
     let uri = activeEditor.document.uri;
     let languageCode = activeEditor.document.languageId;
 
-    // Set the delimiter based on the language code
     if (!this.setDelimiter(languageCode)) {
-      // If the language is not supported, return
       return;
     }
 
-    // Regular expression to match single-line comments (//)
-    const singleLineRegex = /\/\/(.*)/g;
+    for (const delimiter of this.delimiters) {
+      let regexString = delimiter.regexString;
+      let commentType = delimiter.commentType;
+      const regex = new RegExp(regexString, "g");
 
-    // Regular expression to match multi-line comments (/* */ or /** */)
-    const multiLineRegex = /\/\*(.*?)\*\//gs;
+      text = text.replace(regex, (_match, group) => {
+        // First, capitalize nouns within the comment
+        let processedComment = this.capitalizeNoun(group);
+        // Then, capitalize the first letter of the processed comment
+        processedComment = this.capitalizeFirstLetterOfComment(
+          processedComment,
+          commentType
+        );
+        const trimmedComment = processedComment.trim();
+        if (trimmedComment !== "") {
+          return `${commentType} ${trimmedComment}`;
+        } else {
+          return _match;
+        }
+      });
+    }
 
-    // Replace single-line comments
-    text = text.replace(singleLineRegex, (match, group) => {
-      const capitalized =
-        group.trim().charAt(0).toUpperCase() + group.trim().slice(1);
-      return `// ${capitalized}`;
-    });
-
-    // Replace multi-line comments
-    text = text.replace(multiLineRegex, (match, group) => {
-      const capitalized =
-        group.trim().charAt(0).toUpperCase() + group.trim().slice(1);
-      return `/* ${capitalized} */`;
-    });
-
-    // Update the document with the capitalized comments
     this.edit.replace(
       uri,
       new vscode.Range(0, 0, activeEditor.document.lineCount, 0),
@@ -61,11 +62,78 @@ export class Parser {
     );
   }
 
+  // Method to capitalize comment words if they are nouns
+  private capitalizeFirstLetterOfComment(
+    comment: string,
+    commentType: string
+  ): string {
+    // Check if the comment is empty
+    if (!comment) {
+      return comment;
+    }
+
+    // Trim the comment to remove any leading or trailing spaces
+    comment = comment.trim();
+
+    // Find the index of the first space after the comment type
+    const firstSpaceIndex = comment.indexOf(" ");
+
+    // If the comment type is /**/, ensure there's a */ at the end
+    if (commentType === "/*") {
+      // If comment does not end with */
+      if (!comment.endsWith("*/")) {
+        comment += " */";
+      }
+    }
+
+    // If no space is found, return the capitalized comment
+    if (firstSpaceIndex === -1) {
+      return comment.charAt(0).toUpperCase() + comment.slice(1);
+    }
+
+    // Capitalize the first letter of the comment and add one space after the comment type
+    return (
+      comment.charAt(0).toUpperCase() +
+      comment.slice(1, firstSpaceIndex) +
+      " " +
+      comment.slice(firstSpaceIndex + 1)
+    );
+  }
+
+  // Method to capitalise first letter of Noun words
+  private capitalizeNoun(comment: string): string {
+    // Split the comment into words
+    const words = comment.split(/\s+/);
+    for (let i = 0; i < words.length; i++) {
+      if (checkNoun(words[i])) {
+        // Capitalize the first letter of the word
+        words[i] = this.capitalizeFirstLetter(words[i]);
+      }
+    }
+
+    // Check if a space is present before joining the words
+    if (comment.indexOf(" ") !== -1) {
+      // Join the words back into a string with a space
+      return words.join(" ");
+    } else {
+      // Join the words back into a string without a space
+      return words.join("");
+    }
+  }
+
+  // Method to capitalize the first letter of a word
+  private capitalizeFirstLetter(word: string): string {
+    return word.charAt(0).toUpperCase() + word.slice(1);
+  }
+
+  // Method to set delimiters based on language code
   private setDelimiter(languageCode: string): boolean {
+    // Reset properties
     this.supportedLanguage = true;
     this.delimiters = [];
     this.editRanges = [];
 
+    // Switch statement to set delimiters and edit ranges
     switch (languageCode) {
       case "al":
       case "c":
@@ -90,12 +158,17 @@ export class Parser {
       case "swift":
       case "typescript":
       case "typescriptreact":
-        this.delimiters.push("//");
-        this.delimiters.push("/\\*\\*");
+        this.delimiters.push({
+          regexString: "//(.*)",
+          commentType: "//",
+        });
+        this.delimiters.push({
+          regexString: "/\\*(.*?)\\*/",
+          commentType: "/*",
+        });
         this.editRanges.push(true);
         this.editRanges.push(true);
         break;
-
       case "coffeescript":
       case "dockerfile":
       case "elixir":
@@ -110,48 +183,66 @@ export class Parser {
       case "ruby":
       case "shellscript":
       case "yaml":
-        this.delimiters.push("#");
+        this.delimiters.push({
+          regexString: "#(.*)",
+          commentType: "#",
+        });
         this.editRanges.push(true);
         break;
-
       case "ada":
       case "haskell":
       case "plsql":
       case "sql":
       case "lua":
-        this.delimiters.push("--");
+        this.delimiters.push({
+          regexString: "--(.*)",
+          commentType: "--",
+        });
         this.editRanges.push(true);
         break;
-
       case "vb":
-        this.delimiters.push("'");
+        this.delimiters.push({
+          regexString: "'(.*)",
+          commentType: "'",
+        });
         this.editRanges.push(true);
         break;
-
       case "erlang":
       case "latex":
-        this.delimiters.push("%");
+        this.delimiters.push({
+          regexString: "%(.*)",
+          commentType: "%",
+        });
         this.editRanges.push(true);
         break;
-
       case "clojure":
       case "racket":
       case "lisp":
-        this.delimiters.push(";");
+        this.delimiters.push({
+          regexString: ";(.*)",
+          commentType: ";",
+        });
         this.editRanges.push(true);
         break;
-
       case "terraform":
-        this.delimiters.push("#");
+        this.delimiters.push({
+          regexString: "#(.*)",
+          commentType: "#",
+        });
         this.editRanges.push(true);
         break;
-
       case "ACUCOBOL":
       case "OpenCOBOL":
       case "COBOL":
-        this.delimiters.push("\\*>");
+        this.delimiters.push({
+          regexString: "\\*>(.*)",
+          commentType: "\\*",
+        });
+        this.delimiters.push({
+          regexString: "^......\\*(.*)",
+          commentType: "\\*",
+        });
         this.editRanges.push(true);
-        this.delimiters.push("^......\\*");
         this.editRanges.push(false);
         break;
       default:
